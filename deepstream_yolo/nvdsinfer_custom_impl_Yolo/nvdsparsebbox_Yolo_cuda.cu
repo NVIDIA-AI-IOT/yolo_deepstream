a284205f4f482e6b08a89988ae402589d25a4567
Yuwei Wang <yuweiw@nvidia.com>
2023-03-16 16:12:59 +0800

    optimize the deepstream yolov7 accuracy

--- deepstream_yolo/nvdsinfer_custom_impl_Yolo/nvdsparsebbox_Yolo_cuda.cu
+++ deepstream_yolo/nvdsinfer_custom_impl_Yolo/nvdsparsebbox_Yolo_cuda.cu
@@ -53,15 +53,15 @@ __global__ void decodeYoloV7Tensor_cuda(NvDsInferParseObjectInfo *binfo/*output*
         float maxProb = data[ 4];
         //maxProb < Threshold, directly return
         if(maxProb < Threshold){
-            binfo[idx].detectionConfidence = 0;
+            binfo[idx].detectionConfidence = 0.0;
             return;
         }
         float bx = data[ 0];
         float by = data[ 1];
         float bw = data[ 2];
         float bh = data[ 3];
-        int  maxIndex = data[ 5];
-        float * classes_scores = data + 5;
+        int  maxIndex = 0;
+        float * classes_scores = (float *)(data + 5);
         float maxScore = 0;
         int index = 0;
 
@@ -73,12 +73,16 @@ __global__ void decodeYoloV7Tensor_cuda(NvDsInferParseObjectInfo *binfo/*output*
            }
            classes_scores++;
         }
+        if(maxProb * maxScore < Threshold){
+            binfo[idx].detectionConfidence = 0.0;
+            return;
+        }
         maxIndex = index;
         float stride = 1.0;
         float xCenter = bx * stride;
         float yCenter = by * stride;
-        float x0 = xCenter - bw / 2;
-        float y0 = yCenter - bh / 2;
+        float x0 = xCenter - bw / 2.0;
+        float y0 = yCenter - bh / 2.0;
         float x1 = x0 + bw;
         float y1 = y0 + bh;
         x0 = fminf(float(netW), fmaxf(float(0.0), x0));
@@ -89,7 +93,7 @@ __global__ void decodeYoloV7Tensor_cuda(NvDsInferParseObjectInfo *binfo/*output*
         binfo[idx].top = y0;
         binfo[idx].width = fminf(float(netW), fmaxf(float(0.0), x1-x0));
         binfo[idx].height = fminf(float(netH), fmaxf(float(0.0), y1-y0));
-        binfo[idx].detectionConfidence = maxProb;
+        binfo[idx].detectionConfidence = maxProb * maxScore;
         binfo[idx].classId = maxIndex;
     }
     return;
